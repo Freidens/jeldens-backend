1150cdb4ed0d5135c03d59a0e79891d1e95d7e1b
Neil Bacalso <nbofficial15@gmail.com>
2026-07-09 18:09:37 +0800

    Configured links

--- server.js
+++ server.js
@@ -7,15 +7,15 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.redirect('');
+    res.redirect('https://freidens.github.io/jeldens-website/');
 })
 
 app.get('/contact', (req, res) => {
-    res.redirect('');
+    res.redirect('https://freidens.github.io/jeldens-website/src/pages/contact/');
 })
 
 app.get('/store', (req, res) => {
-    res.redirect('');
+    res.redirect('https://freidens.github.io/jeldens-website/src/pages/store/');
 })
 
 app.listen('8000', () => console.log('Server is running.'));
