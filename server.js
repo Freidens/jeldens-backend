6b4fd62f8c705ee2103b67022f85250432e41c11
Neil Bacalso <nbofficial15@gmail.com>
2026-07-09 19:33:02 +0800

    Reverse proxy configured

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
 const app = express();
 const PORT = process.env.PORT
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.redirect('https://api.jeldens.com/');
-})
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
 
-app.get('/contact', (req, res) => {
-    res.redirect('https://api.jeldens.com/');
-})
-
-app.get('/store', (req, res) => {
-    res.redirect('https://api.jeldens.com/');
-})
+app.use('/', createProxyMiddleware({
+    target: "https://freidens.github.io",
+    changeOrigin: true
+}))
 
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
