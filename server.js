1d7193fd6fde59d5db2db147958c9b3f522b273f
Neil Bacalso <nbofficial15@gmail.com>
2026-07-09 18:50:19 +0800

    Fix 1

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.redirect('https://freidens.github.io/jeldens-website/');
+    res.redirect('https://api.jeldens.com/');
 })
 
 app.get('/contact', (req, res) => {
-    res.redirect('https://freidens.github.io/jeldens-website/src/pages/contact/');
+    res.redirect('https://api.jeldens.com/');
 })
 
 app.get('/store', (req, res) => {
-    res.redirect('https://freidens.github.io/jeldens-website/src/pages/store/');
+    res.redirect('https://api.jeldens.com/');
 })
 
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
